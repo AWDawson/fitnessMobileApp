0077bd2d1d4dfffca72ceeed03a1e5143e3e7db1
yshujie <yshujie@umich.edu>
2020-12-02 17:23:10 +0800

    LeanCloud setup

---
+++ Secrets.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCl3NmK88H4MtEctCYt-N0SG2gLvtVIO1E",
+    authDomain: "si669test.firebaseapp.com",
+    databaseURL: "https://si669test.firebaseio.com",
+    projectId: "si669test",
+    storageBucket: "si669test.appspot.com",
+    messagingSenderId: "537529654990",
+    appId: "1:537529654990:web:ae1637fd89667866657a9f"
+};
---
+++ leanCloudConfig.js
@@ -0,0 +1,5 @@
+var config = {
+    appId: "h6CQ4vcqxwVoP9to3zsGhiBk-MdYXbMMI",
+    appKey: "t5sIxJTCiDprqqFTohIaMwEl",
+    // serverURL: "https://please-replace-with-your-customized.domain.com"  
+};
