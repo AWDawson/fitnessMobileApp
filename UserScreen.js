1f4949404e244d3fe1e5455001f7fee5572d9787
WH ZHAO <wenhuipeter98@gmail.com>
2020-12-18 14:57:57 +0800

    update

--- UserScreen.js
+++ UserScreen.js
@@ -12,7 +12,8 @@ import * as adapters from '@leancloud/platform-adapters-react-native';
 import { config } from './leanCloudConfig'
 import Fitness from '@ovalmoney/react-native-fitness';
 import { Pedometer } from 'expo-sensors';
-
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 //const [selectedValue, setSelectedValue] = useState("a");
 
@@ -91,6 +92,9 @@ export class UserScreen extends React.Component {
         'User profile is successfully created!',
         [{ text: 'OK',style: 'OK'}]
       );
+      this.props.navigation.navigate("Home", {
+        mode: "exercise"
+      });
     }, (error) => {
       // 登录失败（可能是密码错误）
       Alert.alert(
@@ -106,176 +110,268 @@ export class UserScreen extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView 
-        style={{
-          flex: 1,
-          backgroundColor: '#00D0BD',
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingTop: 20
-        }}
-        behavior={"height"}
-        keyboardVerticalOffset={10}>
-        <View style={{
-          height: 640,
-      // flex: 0.3,
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '90%',
-      backgroundColor: 'white',
-      opacity: 0.6,
-      borderRadius:20,
-      marginTop: 30,
-      marginBottom: 30,
-    }}>
+      style={{
+        flex: 1,
+        backgroundColor: '#00D0BD',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 20
+      }}
+      behavior={"position"}
+      keyboardVerticalOffset={0}>
+      <View style={{
+        flex: 8,
+    // flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    backgroundColor: 'white',
+    opacity: 0.8,
+    borderRadius:20,
+    marginTop: 40,
+    ...(Platform.OS !== 'android' && {
+      zIndex: 10
+    })
+    // marginBottom: 30,
+  }}>
 
-          <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Email:</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                keyboardType='email-address'
-                autoCapitalize='none'
-                autoCorrect={false}
-                editable={false}
-                selectTextOnFocus={false}
-                value={this.state.emailInput}
-                onChangeText={(text)=>{this.setState({emailInput: text})}}
-              />
+        <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Email:</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              keyboardType='email-address'
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.emailInput}
+              onChangeText={(text)=>{this.setState({emailInput: text})}}
+            />
+        </View>
+      
+       <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Display Name:</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.displayNameInput}
+              onChangeText={(text)=>{this.setState({displayNameInput: text})}}
+            />
+        </View>
+
+        <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Password:</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              textContentType='password'
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.passwordInput}
+              onChangeText={(text)=>{this.setState({passwordInput: text})}}
+            />
+        </View>
+
+        <View  style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            paddingVertical: 13,
+            // borderBottomWidth: 2,
+            width: '100%',
+            ...(Platform.OS !== 'android' && {
+              zIndex: 10
+            })
+        }}>
+            <Text style={loginStyles.inputLabel}>Re-enter Password:</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              textContentType='password'
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.reenterPasswordInput}
+              onChangeText={(text)=>{this.setState({reenterPasswordInput: text})}}
+            />
+        </View>
+
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            paddingVertical: 13,
+            // borderBottomWidth: 2,
+            width: '100%',
+            ...(Platform.OS !== 'android' && {
+              zIndex: 10
+            })
+        }}>
+            <Text style={loginStyles.inputLabel}>Gender:</Text>
+
+            <DropDownPicker
+              items={[
+                  {label: 'Male', value: 'male', hidden: true},
+                  {label: 'Female', value: 'female'},
+              ]}
+              placeholder="Select your gender"
+              // defaultValue={this.state.country}
+              containerStyle={loginStyles.inputTextSignUp}
+              style={{
+                // flex: 0.7,
+                backgroundColor: '#fafafa',
+              }}
+              itemStyle={{
+                  justifyContent: 'flex-start'
+              }}
+              dropDownStyle={{
+                backgroundColor: '#fafafa',
+                ...(Platform.OS !== 'android' && {
+                  zIndex: 10
+                })
+              }}
+              onChangeItem={(itemValue, itemIndex) =>
+                this.setState({genderInput: itemValue})
+              }
+            />
+            {/* <Picker
+            selectedValue={this.state.activeTypeInput}
+            style={{height: 50, width: 100}}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({activeTypeInput: itemValue})
+          }>
+            <Picker.Item label="male" value="male" />
+            <Picker.Item label="female" value="female" />
+            </Picker> */}
           </View>
+
+        {this.state.mode === 'create' ? (
+          <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Age:</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              keyboardType={'numeric'}
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.ageInput}
+              onChangeText={(text)=>{this.setState({ageInput:  text.replace(/[^0-9]/g, '')})}}
+            />
+          </View>
+        ):(
+          <View/>
+        )}
+        {this.state.mode === 'create' ? (
+          <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Weight (kg):</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.weightInput}
+              onChangeText={(text)=>{this.setState({weightInput:  text.replace(/[^0-9]/g, '')})}}
+            />
+          </View>
+        ):(
+          <View/>
+        )}
+        {this.state.mode === 'create' ? (
+          <View style={loginStyles.inputRowSignUp}>
+            <Text style={loginStyles.inputLabel}>Height (cm):</Text>
+            <TextInput
+              style={loginStyles.inputTextSignUp}
+              autoCapitalize='none'
+              autoCorrect={false}
+              value={this.state.heightInput}
+              onChangeText={(text)=>{this.setState({heightInput:  text.replace(/[^0-9]/g, '')})}}
+            />
+          </View>
+        ):(
+          <View/>
+        )}
+        {/* ActiveType tells you whether a user is active in sports
+            Implement a dropdown menu which user can select their types (accroding to the following fashion)
+            value: little or no exercise  ->  activeTypeInput: sedentary
+            value: light exercise/sports 1-3 days/week  ->  activeTypeInput: lightly active
+            value: moderate exercise/sports 3-5 days/week  ->  activeTypeInput: moderately active
+            value: hard exercise/sports 6-7 days a week  ->  activeTypeInput: very active
+            value: very hard exercise/sports & physical job or 2x training  ->  activeTypeInput: extra active
+
+        */}
         
-         <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Display Name:</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.displayNameInput}
-                onChangeText={(text)=>{this.setState({displayNameInput: text})}}
-              />
-          </View>
+          <View  style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            paddingVertical: 13,
+            // borderBottomWidth: 2,
+            width: '100%',
+            ...(Platform.OS !== 'android' && {
+              zIndex: 10
+            })
+        }}>
+            <Text style={loginStyles.inputLabel}>Active Type:</Text>
 
-          <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Password:</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                textContentType='password'
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.passwordInput}
-                onChangeText={(text)=>{this.setState({passwordInput: text})}}
-              />
-          </View>
-
-          <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Re-enter Password:</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                textContentType='password'
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.reenterPasswordInput}
-                onChangeText={(text)=>{this.setState({reenterPasswordInput: text})}}
-              />
-          </View>
-
-         
-            <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Gender:</Text>
-              <Picker
-              selectedValue={this.state.activeTypeInput}
-              style={{height: 50, width: 100}}
-              onValueChange={(itemValue, itemIndex) =>
+            <DropDownPicker
+              items={[
+                  {label: 'Little or no exercise', value: 'sedentary', hidden: true},
+                  {label: 'Light exercise/sports 1-3 days/week', value: 'activeTypeInput: lightly active'},
+                  {label: 'Moderate exercise/sports 3-5 days/week', value: 'activeTypeInput: lightly active'},
+                  {label: 'Hard exercise/sports 6-7 days a week', value: 'activeTypeInput: lightly active'},
+                  {label: 'Very hard exercise/sports & physical job or 2x training', value: 'activeTypeInput: extra active'},
+              ]}
+              placeholder="Select your type"
+              // defaultValue={this.state.country}
+              containerStyle={loginStyles.inputTextSignUp}
+              style={{
+                backgroundColor: '#fafafa',
+              }}
+              itemStyle={{
+                  justifyContent: 'flex-start'
+              }}
+              dropDownStyle={{backgroundColor: '#fafafa'}}
+              onChangeItem={(itemValue, itemIndex) =>
                 this.setState({activeTypeInput: itemValue})
-            }>
-              <Picker.Item label="male" value="male" />
-              <Picker.Item label="female" value="female" />
-              </Picker>
-            </View>
-          
-          
-            <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Age:</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                keyboardType={'numeric'}
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.ageInput}
-                onChangeText={(text)=>{this.setState({ageInput:  text.replace(/[^0-9]/g, '')})}}
-              />
-            </View>
-          
+              }
+            />
+          {/* <Picker
+            selectedValue={this.state.activeTypeInput}
+            style={{height: 50, width: 100}}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({activeTypeInput: itemValue})
+          }>
+            <Picker.Item label="little or no exercise" value="sedentary" />
+            <Picker.Item label="light exercise/sports 1-3 days/week" value="activeTypeInput: lightly active" />
+            <Picker.Item label="moderate exercise/sports 3-5 days/week" value="activeTypeInput: lightly active" />
+            <Picker.Item label="hard exercise/sports 6-7 days a week" value="activeTypeInput: lightly active" />
+            <Picker.Item label="very hard exercise/sports & physical job or 2x training" value="activeTypeInput: extra active" />
+
+          </Picker> */}
+           
+          </View>
+
+
+      </View>
+      
+ 
          
-            <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Weight (kg):</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.weightInput}
-                onChangeText={(text)=>{this.setState({weightInput:  text.replace(/[^0-9]/g, '')})}}
-              />
-            </View>
-          
-            <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Height (cm):</Text>
-              <TextInput
-                style={loginStyles.inputText}
-                autoCapitalize='none'
-                autoCorrect={false}
-                value={this.state.heightInput}
-                onChangeText={(text)=>{this.setState({heightInput:  text.replace(/[^0-9]/g, '')})}}
-              />
-            </View>
-          
-          {/* ActiveType tells you whether a user is active in sports
-              Implement a dropdown menu which user can select their types (accroding to the following fashion)
-              value: little or no exercise  ->  activeTypeInput: sedentary
-              value: light exercise/sports 1-3 days/week  ->  activeTypeInput: lightly active
-              value: moderate exercise/sports 3-5 days/week  ->  activeTypeInput: moderately active
-              value: hard exercise/sports 6-7 days a week  ->  activeTypeInput: very active
-              value: very hard exercise/sports & physical job or 2x training  ->  activeTypeInput: extra active
+      <View style={loginStyles.bottomViewSignUp}> 
+        <TouchableOpacity 
+              style={loginStyles.buttonContainerLogIn2}
+              onPress={()=>{
+                this.props.navigation.navigate("Home", {
+                  mode:'exericse'
+                })
+              }}
+              >
+              <Text style={loginStyles.buttonTextLogIn}>Cancel</Text>
+        </TouchableOpacity>  
 
-          */}
-          
-            <View style={loginStyles.inputRow}>
-              <Text style={loginStyles.inputLabel}>Active Type:</Text>
-            <Picker
-              selectedValue={this.state.activeTypeInput}
-              style={{height: 50, width: 100}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({activeTypeInput: itemValue})
-            }>
-              <Picker.Item label="little or no exercise" value="sedentary" />
-              <Picker.Item label="light exercise/sports 1-3 days/week" value="activeTypeInput: lightly active" />
-              <Picker.Item label="moderate exercise/sports 3-5 days/week" value="activeTypeInput: lightly active" />
-              <Picker.Item label="hard exercise/sports 6-7 days a week" value="activeTypeInput: lightly active" />
-              <Picker.Item label="very hard exercise/sports & physical job or 2x training" value="activeTypeInput: extra active" />
-
-        </Picker>
-             
-            </View>
-
-          <TouchableOpacity 
-              style={{ 
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 30,
-              backgroundColor: 'white',
-              width: 300,
-              height: 65,
-              marginTop: 10,
-            }}
-              onPress={this.onCreateAccount}
+        <TouchableOpacity 
+              style={loginStyles.buttonContainerSignUp2}
+              onPress={this.onUpdateAccount}
               >
               <Text style={loginStyles.buttonTextSignUp}>Save</Text>
-            </TouchableOpacity>
-        </View>
-        
-        
-           
- 
-       
-      </KeyboardAvoidingView>
-    )
+        </TouchableOpacity>  
+      </View>
+     
+
+    </KeyboardAvoidingView>
+  )
   }
 }
